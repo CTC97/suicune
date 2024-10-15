19356483d2e29cebee506b51b46fb83cbca0f69a
CTC97 <pixelq6@gmail.com>
2024-10-14 22:20:46 -0400

    game loop mock

--- src/suicune-client.cr
+++ src/suicune-client.cr
@@ -24,6 +24,7 @@ class Client
         @suicune.alpha_mod = 88
         # @suicune = @suicune.convert(@window.surface)
 
+        @running = true
 
         @suicune_x = 10
         @suicune_y = 10
@@ -40,57 +41,64 @@ class Client
     end
 
     def run
-        index = 0
-        loop do
-
-            puts "(#{@suicune_x}, #{@suicune_y})"
-
-            case event = SDL::Event.wait
+        while @running
+            while event = SDL::Event.poll
+            case event
             when SDL::Event::Quit
-                break
+                @running = false
             when SDL::Event::Keyboard
                 case event.sym
                 when .a?
-                    if @suicune_x - @suicune_speed - @bounds >= 0
-                        @suicune_x -= @suicune_speed
-                    end
-                when .d?
-                    if @suicune_x + @suicune.width + @suicune_speed + @bounds <= @window.width
-                        @suicune_x += @suicune_speed
-                    end
-                when .s?
-                    if @suicune_y + @suicune.height + @suicune_speed + @bounds <= @window.height
-                        @suicune_y += @suicune_speed
-                    end
-                when .w?
-                    if @suicune_y - @bounds - @suicune_speed >= 0
-                        @suicune_y -= @suicune_speed
-                    end
+                if @suicune_x - @suicune_speed - @bounds >= 0
+                    @suicune_x -= @suicune_speed
                 end
+                when .d?
+                if @suicune_x + @suicune.width + @suicune_speed + @bounds <= @window.width
+                    @suicune_x += @suicune_speed
+                end
+                when .s?
+                if @suicune_y + @suicune.height + @suicune_speed + @bounds <= @window.height
+                    @suicune_y += @suicune_speed
+                end
+                when .w?
+                if @suicune_y - @bounds - @suicune_speed >= 0
+                    @suicune_y -= @suicune_speed
+                end
+                end
+            end
             end
 
             clear_screen
-            @client << "#{index}\n"
-            # index += 1
-            # response = @client.gets
-            # puts response
 
-            # for srcrect, dstrect - https://stackoverflow.com/questions/57304912/purpose-of-sdl-rect-structure-in-sdl-2-0
+            # suicune
             @renderer.copy(@suicune, nil, SDL::Rect[@suicune_x, @suicune_y, 64, 64])
 
-            # display nurse joy
+            # nurse joy
             current_clip = @nurse_joy_frames[(@nurse_joy_frame / @slowdown).to_i]
-            puts current_clip
             x = (@window.width - current_clip.w) / 2
             y = (@window.height - current_clip.h) / 2
             @renderer.copy(@nurse_joy, current_clip, SDL::Rect[x.to_i, y.to_i, current_clip.w, current_clip.h])
 
             @renderer.present
+
             @nurse_joy_frame = (@nurse_joy_frame + 1) % (@nurse_joy_frames.size * @slowdown)
 
-            # @suicune.blit(@window.surface)
+
+            # psuedocode for game loop:
+
+            # Frame rate management
+            #current_time = SDL::get_ticks
+            #frame_time = current_time - last_frame_time
+
+            #if frame_delay > frame_time
+            #SDL::delay(frame_delay - frame_time)
+            #end
+
+            #last_frame_time = SDL::get_ticks
+            sleep(1/60)
         end
     end
+
 
     private def clear_screen
         @renderer.draw_color = SDL::Color[208, 236, 234, 255] # clear screen in white
